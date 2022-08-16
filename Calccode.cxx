db52403da904f69daa1bdc39ce32bf565751e7ca
Presh-Codes <preciousokeypraise@gmail.com>
2022-08-16 16:31:11 +0100

    origin

--- Calccode.cxx
+++ Calccode.cxx
@@ -5,32 +5,37 @@ int main()
     int a, b, c, d;
     std::cout<<"Hello there, what operation would you like to perform?\n1. Addition\n2. Subtraction\n3. Division\n4. Multiplication\n";
     std::cin>>a;
-    if(a==1){
-        std::cout<<"Type in a number";
-        std::cin>>b;
-        std::cout<<"Type in another number";
-        std::cin>>c;
-        std::cout<<"Your sum is "<< b+c;
-    }
-    if(a==2){
-        std::cout<<"Type in a number";
-        std::cin>>b;
-        std::cout<<"Type in another number";
-        std::cin>>c;
-        std::cout<<"Your answer is "<< b-c;
-    }
-    if(a==3){
-        std::cout<<"Type in a number";
-        std::cin>>b;
-        std::cout<<"Type in another number";
-        std::cin>>c;
-        std::cout<<"Your answer is "<< b/c;
-    }
-    if(a==4){
-        std::cout<<"Type in a number";
-        std::cin>>b;
-        std::cout<<"Type in another number";
-        std::cin>>c;
-        std::cout<<"Your answer is "<< b*c;
+    switch(a){
+        case 1:
+            std::cout<<"Type in a number";
+            std::cin>>b;
+            std::cout<<"Type in another number";
+            std::cin>>c;
+            std::cout<<"Your sum is "<< b+c;
+            break;
+        case 2:
+            std::cout<<"Type in a number";
+            std::cin>>b;
+            std::cout<<"Type in another number";
+            std::cin>>c;
+            std::cout<<"Your answer is "<< b-c;
+            break;
+        case 3:
+            std::cout<<"Type in a number";
+            std::cin>>b;
+            std::cout<<"Type in another number";
+            std::cin>>c;
+            std::cout<<"Your answer is "<< b/c;
+            break;
+        case 4:
+            std::cout<<"Type in a number";
+            std::cin>>b;
+            std::cout<<"Type in another number";
+            std::cin>>c;
+            std::cout<<"Your answer is "<< b*c;
+            break;
+        default:
+            std::cout<<"That's not part of the options!";
+        
     }
 }
