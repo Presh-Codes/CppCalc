d60273865b21b9336c13f79674874a1db4e16f48
Praisecodes <cmancoder@gmail.com>
2022-08-16 16:48:11 +0100

    Update: Added Functions Fix Folder && Created Readme

---
+++ Solve With Functions/functionsolve.cxx
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int add(int num1, int num2);
+int subtract(int num1, int num2);
+int divide(int num1, int num2);
+int multiply(int num1, int num2);
+
+int main(){
+    int a, b, c, d;
+    std::cout<<"Hello there, what operation would you like to perform?\n1. Addition\n2. Subtraction\n3. Division\n4. Multiplication\n";
+}
