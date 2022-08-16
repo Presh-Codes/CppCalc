f00c078cfc711f46b1cafff514ed19898248670e
Praisecodes <cmancoder@gmail.com>
2022-08-16 17:00:54 +0100

    Update: Code Completion && Update Readme

--- Solve With Functions/functionsolve.cxx
+++ Solve With Functions/functionsolve.cxx
@@ -6,6 +6,49 @@ int divide(int num1, int num2);
 int multiply(int num1, int num2);
 
 int main(){
-    int a, b, c, d;
+    int choice, FirstNumber, SecondNumber;
     std::cout<<"Hello there, what operation would you like to perform?\n1. Addition\n2. Subtraction\n3. Division\n4. Multiplication\n";
+    std::cin >> choice;
+    std::cout << "Enter Your First Number: ";
+    std::cin >> FirstNumber;
+    std::cout << "Enter Your Second Number: ";
+    std::cin >> SecondNumber;
+    switch(choice){
+        case 1:
+            add(FirstNumber, SecondNumber);
+            break;
+        case 2:
+            subtract(FirstNumber, SecondNumber);
+            break;
+        case 3:
+            if(!(SecondNumber == 0)){
+                divide(FirstNumber, SecondNumber);
+            }
+            else{
+                std::cout << "You cannot divide by zero\n";
+            }
+            break;
+        case 4:
+            multiply(FirstNumber, SecondNumber);
+            break;
+        default:
+            std::cout << "That's not part of the options\n";
+    }
+    return 0;
+}
+
+int add(int num1, int num2){
+    return (num1+num2);
+}
+
+int subtract(int num1, int num2){
+    return (num1-num2);
+}
+
+int divide(int num1, int num2){
+    return (num1/num2);
+}
+
+int multiply(int num1, int num2){
+    return (num1*num2);
 }
--- Solve With Functions/readme.md
+++ Solve With Functions/readme.md
@@ -5,3 +5,5 @@
 - Removed unnecessary variable (d) from your code.
 - Used functions to make calculations, for code readabilit (because code readability is very important).
 - Changed variable names to be a lil bit more specific so other programmers will know exactly what each variable does.
+- Eliminated code repition spotted in [Calccode.cxx](https://github.com/Presh-Codes/CppCalc/blob/main/Calccode.cxx)
+- Added check for division by zero.
